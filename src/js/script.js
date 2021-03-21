deae9b98ae818d41e6e3e4d06b5665e519e3e9cf
Piotr Stepien <naz@o2.pl>
2021-03-21 17:41:27 +0100

    Add class BooksList, chenged functions to methods

--- src/js/script.js
+++ src/js/script.js
@@ -9,53 +9,88 @@
     },
     containerOf: {
       books: '.books-list',
-      images: '.books-list .book__image',
+    },
+    book: {
+      image: '.books-list .book__image',
     },
   };
 
   const templates = {
-    books: Handlebars.compile(document.querySelector(select.templateOf.books).innerHTML),
+    books: Handlebars.compile(
+      document.querySelector(select.templateOf.books).innerHTML
+    ),
   };
 
-  const favoriteBooks = [];
+  class BooksList {
+    constructor() {
+      const thisBooksList = this; //Pytanie 1: jak używać this jako const bez przypisywania do nowej nazwy? Czyli żeby używać wszędzie this zamiast thisNazwa. Jak nie zadeklarowałem z nazwą to zwracało mi, że tablice favoriteBooks jest undefined.
 
-  function render() {
-    //console.log(document.querySelector(select.templateOf.books));
-    for(let book of dataSource.books){
-      const bookHTML = templates.books(book);
-      //console.log('bookHTML',bookHTML);
+      thisBooksList.initData();
+      thisBooksList.getElements();
+      thisBooksList.render();
+      thisBooksList.initActions();
+    }
 
-      const bookDOM = utils.createDOMFromHTML(bookHTML);
-      //console.log('bookDOM',bookDOM);
+    initData() {
+      const thisBooksList = this;
+      thisBooksList.data = dataSource.books;
+    }
 
-      const bookContainer = document.querySelector(select.containerOf.books);
-      //console.log('bookContainer',bookContainer);
-      bookContainer.appendChild(bookDOM);
+    getElements() {
+      const thisBooksList = this;
+
+      thisBooksList.favoriteBooks = []; //Pytanie 1: jak używać this jako const bez przypisywania do nowej nazwy? Czyli żeby używać wszędzie this zamiast thisNazwa. Jak nie zadeklarowałem z nazwą to zwracało mi, że tablice favoriteBooks jest undefined.
+
+      thisBooksList.bookContainer = document.querySelector(
+        select.containerOf.books
+      );
+    }
+
+    render() {
+      const thisBooksList = this;
+      //console.log(document.querySelector(select.templateOf.books));
+      for (let book of thisBooksList.data) {
+        const bookHTML = templates.books(book);
+        //console.log('bookHTML',bookHTML);
+
+        const bookDOM = utils.createDOMFromHTML(bookHTML);
+        //console.log('bookDOM',bookDOM);
+
+        //console.log('bookContainer',bookContainer);
+        thisBooksList.bookContainer.appendChild(bookDOM);
+      }
+    }
+
+    initActions() {
+      const thisBooksList = this;
+
+      const images = document.querySelectorAll(select.book.image);
+      for (let image of images) {
+        //console.log(image);
+        image.addEventListener('dblclick', function (event) {
+          event.preventDefault();
+          //console.log(image.data-id);
+          image.classList.toggle('favorite');
+          let imageID = image.getAttribute('data-id');
+
+          if (!thisBooksList.favoriteBooks.includes(imageID))
+            thisBooksList.favoriteBooks.push(imageID);
+          else if (thisBooksList.favoriteBooks.includes(imageID))
+            thisBooksList.favoriteBooks.splice(
+              thisBooksList.favoriteBooks.indexOf(imageID, 1)
+            );
+          console.log(thisBooksList.favoriteBooks);
+        });
+      }
+      //console.log("test");
     }
   }
 
-  function initActions() {
+  const app = {
+    init: function () {
+      new BooksList();
+    },
+  };
 
-    const imageContainer = document.querySelectorAll(select.containerOf.images);
-    //console.log(imageContainer);
-    for (let image of imageContainer) {
-      //imageContainer = document.querySelector(select.containerOf.image);
-      //console.log(image);
-      image.addEventListener('dblclick', function (event) {
-        event.preventDefault();
-        //console.log(image.data-id);
-        image.classList.toggle('favorite');
-        let imageID = image.getAttribute('data-id');
-        if(!favoriteBooks.includes(imageID))favoriteBooks.push(imageID);
-        else if(favoriteBooks.includes(imageID)) favoriteBooks.splice(favoriteBooks.indexOf(imageID, 1));
-        //console.log(favoriteBooks);
-      });
-    }
-    //console.log("test");
-
-
-  }
-
-  render();
-  initActions();
+  app.init();
 }
